159883341469ef5e11161549fb57013950a7408d
narasimha-1511 <s.narasimha.2005@gmail.com>
2025-07-25 01:21:12 +0530

    learned how to write the transaction instructions

--- client/index.test.ts
+++ client/index.test.ts
@@ -8,6 +8,7 @@ import {
 	SystemProgram,
 	Keypair,
 	LAMPORTS_PER_SOL,
+    TransactionInstruction,
 } from "@solana/web3.js";
 
 test("intialized data account", () => {
@@ -22,7 +23,7 @@ test("intialized data account", () => {
 	svm.airdrop(payer.publicKey, BigInt(LAMPORTS_PER_SOL));
 	
     const dataAccount = new Keypair();
-	const blockhash = svm.latestBlockhash();
+	let blockhash = svm.latestBlockhash();
 	
     const ixs = [
 		SystemProgram.createAccount({
@@ -33,13 +34,55 @@ test("intialized data account", () => {
             programId: contractPubKey.publicKey
 		}),
 	];
+
+    // const ixs = new TransactionInstruction({
+    //     keys:[
+    //         {pubkey: dataAccount.publicKey , isSigner: true , isWritable: true}
+    //     ],
+    //     programId: contractPubKey.publicKey,
+    //     data: Buffer.from("")
+    // });
 	
     const tx = new Transaction();
 	tx.recentBlockhash = blockhash;
     tx.feePayer = payer.publicKey;
+	// tx.add(ixs);
 	tx.add(...ixs);
 	tx.sign(payer,dataAccount);
 	svm.sendTransaction(tx);
 	const balanceAfter = svm.getBalance(dataAccount.publicKey);
 	expect(balanceAfter).toBe(svm.minimumBalanceForRentExemption(4n));
+
+    //the first transaciton is done brother
+function dob(){
+
+    const ix2 = new TransactionInstruction({
+        keys:[
+            {pubkey: dataAccount.publicKey , isSigner: false , isWritable: true}
+        ],
+        programId: contractPubKey.publicKey,
+        data: Buffer.from("")
+    });
+
+    const tx2 = new Transaction();
+    tx2.recentBlockhash = svm.latestBlockhash();
+    tx2.feePayer = payer.publicKey;
+    tx2.add(ix2);
+    tx2.sign(payer);
+    svm.sendTransaction(tx2);
+    svm.expireBlockhash()
+    }
+
+    dob();
+    dob();
+    dob();
+    dob();
+
+    const updatedDataAcccount = svm.getAccount(dataAccount.publicKey);
+        
+    expect(updatedDataAcccount?.data[0]).toBe(8);
+    expect(updatedDataAcccount?.data[1]).toBe(0);
+    expect(updatedDataAcccount?.data[2]).toBe(0);
+    expect(updatedDataAcccount?.data[3]).toBe(0);
+
 });
